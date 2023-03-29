8ced654c2335398d8a836751b7953d95c9823880
Deepashri Dali <82769106+DeepashriDali@users.noreply.github.com>
2023-03-29 21:56:28 +0100

    remove hash in hexcode

--- script.js
+++ script.js
@@ -8,38 +8,43 @@ const randColor = () => {
       .toUpperCase()
   );
 };
+let div1 = document.querySelector("#clr1");
+let text1 = document.querySelector("#text1");
+let div2 = document.querySelector("#clr2");
+let text2 = document.querySelector("#text2");
+let div3 = document.querySelector("#clr3");
+let text3 = document.querySelector("#text3");
+let div4 = document.querySelector("#clr4");
+let text4 = document.querySelector("#text4");
+let div5 = document.querySelector("#clr5");
+let text5 = document.querySelector("#text5");
 
 // console.log(randColor());
 let generateRandomColour = () => {
-  let div1 = document.querySelector("#clr1");
-  let text1 = document.querySelector("#text1");
   let div1Clr = randColor();
   div1.style.backgroundColor = div1Clr;
-  text1.innerHTML = `${div1Clr}`;
+  let text1sliced = div1Clr.slice(1);
+  text1.innerHTML = `${text1sliced}`;
 
-  let div2 = document.querySelector("#clr2");
-  let text2 = document.querySelector("#text2");
   let div2Clr = randColor();
   div2.style.backgroundColor = div2Clr;
-  text2.innerHTML = `${div2Clr}`;
+  let text2sliced = div2Clr.slice(1);
+  text2.innerHTML = `${text2sliced}`;
 
-  let div3 = document.querySelector("#clr3");
-  let text3 = document.querySelector("#text3");
   let div3Clr = randColor();
   div3.style.backgroundColor = div3Clr;
-  text3.innerHTML = `${div3Clr}`;
+  let text3sliced = div3Clr.slice(1);
+  text3.innerHTML = `${text3sliced}`;
 
-  let div4 = document.querySelector("#clr4");
-  let text4 = document.querySelector("#text4");
   let div4Clr = randColor();
   div4.style.backgroundColor = div4Clr;
-  text4.innerHTML = `${div4Clr}`;
+  let text4sliced = div4Clr.slice(1);
+  text4.innerHTML = `${text4sliced}`;
 
-  let div5 = document.querySelector("#clr5");
-  let text5 = document.querySelector("#text5");
   let div5Clr = randColor();
   div5.style.backgroundColor = div5Clr;
-  text5.innerHTML = `${div5Clr}`;
+  let text5sliced = div5Clr.slice(1);
+  text5.innerHTML = `${text5sliced}`;
 };
 generateRandomColour();
 
