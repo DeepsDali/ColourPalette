125005597ddef5992333a661f885898f20bd156e
Deepashri Dali <82769106+DeepashriDali@users.noreply.github.com>
2023-03-29 14:19:45 +0100

    generate random colours

---
+++ script.js
@@ -0,0 +1,26 @@
+//generate random color
+const randColor = () => {
+  return (
+    "#" +
+    Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, "0")
+      .toUpperCase()
+  );
+};
+
+// console.log(randColor());
+let div1 = document.querySelector("#clr1");
+div1.style.backgroundColor = randColor();
+
+let div2 = document.querySelector("#clr2");
+div2.style.backgroundColor = randColor();
+
+let div3 = document.querySelector("#clr3");
+div3.style.backgroundColor = randColor();
+
+let div4 = document.querySelector("#clr4");
+div4.style.backgroundColor = randColor();
+
+let div5 = document.querySelector("#clr5");
+div5.style.backgroundColor = randColor();
