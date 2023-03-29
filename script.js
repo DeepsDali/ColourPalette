639d002c2ff39345279629b98b172858a6618e0e
Deepashri Dali <82769106+DeepashriDali@users.noreply.github.com>
2023-03-29 15:36:20 +0100

    generate colours with space bar press

--- script.js
+++ script.js
@@ -10,17 +10,28 @@ const randColor = () => {
 };
 
 // console.log(randColor());
-let div1 = document.querySelector("#clr1");
-div1.style.backgroundColor = randColor();
+let generateRandomColour = () => {
+  let div1 = document.querySelector("#clr1");
+  div1.style.backgroundColor = randColor();
 
-let div2 = document.querySelector("#clr2");
-div2.style.backgroundColor = randColor();
+  let div2 = document.querySelector("#clr2");
+  div2.style.backgroundColor = randColor();
 
-let div3 = document.querySelector("#clr3");
-div3.style.backgroundColor = randColor();
+  let div3 = document.querySelector("#clr3");
+  div3.style.backgroundColor = randColor();
 
-let div4 = document.querySelector("#clr4");
-div4.style.backgroundColor = randColor();
+  let div4 = document.querySelector("#clr4");
+  div4.style.backgroundColor = randColor();
 
-let div5 = document.querySelector("#clr5");
-div5.style.backgroundColor = randColor();
+  let div5 = document.querySelector("#clr5");
+  div5.style.backgroundColor = randColor();
+};
+generateRandomColour();
+
+// event = keyup or keydown
+document.addEventListener("keyup", (event) => {
+  if (event.code === "Space") {
+    console.log("Space pressed");
+    generateRandomColour();
+  }
+});
