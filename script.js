6eddbd8cd303384fb5ff042bfe56ee9c48119e90
Deepashri Dali <82769106+DeepashriDali@users.noreply.github.com>
2023-03-31 18:49:04 +0100

    Add function to check contrast ratio and alter text color of div 1 accordingly and display text in uppercase

--- script.js
+++ script.js
@@ -55,26 +55,56 @@ document.addEventListener("keyup", (event) => {
     generateRandomColour();
   }
 });
-
+//
 let colorPickerButton = document.getElementById("text1");
 let colorInput = document.getElementById("color-picker");
 
 colorPickerButton.addEventListener("click", function () {
   colorInput.click();
-  let selectedColour = colorPicker.value;
-  console.log(selectedColour);
-  div1.style.backgroundColor = selectedColour;
 });
 
 colorInput.addEventListener("input", () => {
   let selectedColour = colorInput.value;
+  console.log(hexToRgb(selectedColour));
+  let rgbColor = hexToRgb(selectedColour);
+  console.log(rgbColor);
+  let selectedColorLuminance =
+    luminance(rgbColor.r, rgbColor.g, rgbColor.b) + 0.05;
+  console.log(selectedColorLuminance);
+  let contrastRatio = 1.05 / selectedColorLuminance;
+  console.log(`cr = ${contrastRatio}`);
+  contrastRatio > 3
+    ? (text1.style.color = "#FFFFFF")
+    : (text1.style.color = "#000000");
   div1.style.backgroundColor = selectedColour;
   text1sliced = selectedColour.slice(1);
-  text1.innerHTML = `${text1sliced}`;
+  text1.innerHTML = `${text1sliced.toUpperCase()}`;
 });
+// function to convert hex to rgb
+let hexToRgb = (hex) => {
+  var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  hex = hex.replace(shorthandRegex, function (m, r, g, b) {
+    return r + r + g + g + b + b;
+  });
 
-// let changeColour = () => {
-//   let colour = document.querySelector("#inputColour1").value;
-//   div1.style.backgroundColor = colour;
-//   text1.innerHTML = `${colour}`;
-// };
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result
+    ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16),
+      }
+    : null;
+};
+// function to calculat luminance
+
+let luminance = (r, g, b) => {
+  var a = [r, g, b].map(function (v) {
+    v /= 255;
+    return v <= 0.03928 ? v / 12.92 : Math.pow((v + 0.055) / 1.055, 2.4);
+  });
+  return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722;
+};
+console.log(luminance(255, 255, 255) + 0.05);
+console.log(luminance(224, 16, 16) + 0.05);
+console.log(luminance(240, 239, 239) + 0.05);
