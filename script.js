04c07cc161a77985b3257b0d5af20902cc4f98c0
Deepashri Dali <82769106+DeepashriDali@users.noreply.github.com>
2023-03-31 20:32:24 +0100

    Refactor generate random colour function

--- script.js
+++ script.js
@@ -8,6 +8,38 @@ const randColor = () => {
       .toUpperCase()
   );
 };
+
+// function to convert hex to rgb
+let hexToRgb = (hex) => {
+  var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  hex = hex.replace(shorthandRegex, function (m, r, g, b) {
+    return r + r + g + g + b + b;
+  });
+
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result
+    ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16),
+      }
+    : null;
+};
+// function to calculate luminance
+
+let luminance = (r, g, b) => {
+  var a = [r, g, b].map(function (v) {
+    v /= 255;
+    return v <= 0.03928 ? v / 12.92 : Math.pow((v + 0.055) / 1.055, 2.4);
+  });
+  return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722;
+};
+console.log(luminance(255, 255, 255) + 0.05);
+console.log(luminance(224, 16, 16) + 0.05);
+console.log(luminance(240, 239, 239) + 0.05);
+
+//assign variables
+
 let div1 = document.querySelector("#clr1");
 let text1 = document.querySelector("#text1");
 let div2 = document.querySelector("#clr2");
@@ -20,39 +52,38 @@ let div5 = document.querySelector("#clr5");
 let text5 = document.querySelector("#text5");
 
 // console.log(randColor());
-let generateRandomColour = () => {
-  let div1Clr = randColor();
-  div1.style.backgroundColor = div1Clr;
-  let text1sliced = div1Clr.slice(1);
-  text1.innerHTML = `${text1sliced}`;
 
-  let div2Clr = randColor();
-  div2.style.backgroundColor = div2Clr;
-  let text2sliced = div2Clr.slice(1);
-  text2.innerHTML = `${text2sliced}`;
-
-  let div3Clr = randColor();
-  div3.style.backgroundColor = div3Clr;
-  let text3sliced = div3Clr.slice(1);
-  text3.innerHTML = `${text3sliced}`;
-
-  let div4Clr = randColor();
-  div4.style.backgroundColor = div4Clr;
-  let text4sliced = div4Clr.slice(1);
-  text4.innerHTML = `${text4sliced}`;
-
-  let div5Clr = randColor();
-  div5.style.backgroundColor = div5Clr;
-  let text5sliced = div5Clr.slice(1);
-  text5.innerHTML = `${text5sliced}`;
+let generateRandomColour = (div, text) => {
+  let divClr = randColor();
+  div.style.backgroundColor = divClr;
+  let textsliced = divClr.slice(1);
+  text.innerHTML = `${textsliced}`;
+  //   change text colour
+  let rgbColor = hexToRgb(divClr);
+  let selectedColorLuminance =
+    luminance(rgbColor.r, rgbColor.g, rgbColor.b) + 0.05;
+  let contrastRatio = 1.05 / selectedColorLuminance;
+  contrastRatio > 3
+    ? (text.style.color = "#FFFFFF")
+    : (text.style.color = "#000000");
 };
-generateRandomColour();
+
+generateRandomColour(div1, text1);
+generateRandomColour(div2, text2);
+generateRandomColour(div3, text3);
+generateRandomColour(div4, text4);
+generateRandomColour(div5, text5);
 
 // event = keyup or keydown
 document.addEventListener("keyup", (event) => {
   if (event.code === "Space") {
     console.log("Space pressed");
-    generateRandomColour();
+    // generateRandomColour();
+    generateRandomColour(div1, text1);
+    generateRandomColour(div2, text2);
+    generateRandomColour(div3, text3);
+    generateRandomColour(div4, text4);
+    generateRandomColour(div5, text5);
   }
 });
 //
@@ -80,31 +111,3 @@ colorInput.addEventListener("input", () => {
   text1sliced = selectedColour.slice(1);
   text1.innerHTML = `${text1sliced.toUpperCase()}`;
 });
-// function to convert hex to rgb
-let hexToRgb = (hex) => {
-  var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-  hex = hex.replace(shorthandRegex, function (m, r, g, b) {
-    return r + r + g + g + b + b;
-  });
-
-  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  return result
-    ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16),
-      }
-    : null;
-};
-// function to calculat luminance
-
-let luminance = (r, g, b) => {
-  var a = [r, g, b].map(function (v) {
-    v /= 255;
-    return v <= 0.03928 ? v / 12.92 : Math.pow((v + 0.055) / 1.055, 2.4);
-  });
-  return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722;
-};
-console.log(luminance(255, 255, 255) + 0.05);
-console.log(luminance(224, 16, 16) + 0.05);
-console.log(luminance(240, 239, 239) + 0.05);
